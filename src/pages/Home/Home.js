f2536c5c60cb59c9b1fc23facdb07c14dce4bd29
Devin Castro <dcrocker21@gmail.com>
2020-10-19 15:05:44 -0700

    added photo gallery

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,6 +3,18 @@ import '../../index.css'
 import { Container, Row, Col, Button, Jumbotron } from 'reactstrap'
 import cleaner from '../../lewisphotos/cleanerIcon.png'
 import solar from '../../lewisphotos/solarIcon.png'
+import { AnimateOnChange } from 'react-animation'
+import photo1 from '../../lewisphotos/unnamed-1.jpg'
+import photo2 from '../../lewisphotos/unnamed-2.jpg'
+import photo3 from '../../lewisphotos/unnamed-3.jpg'
+import photo4 from '../../lewisphotos/unnamed-4.jpg'
+import photo5 from '../../lewisphotos/unnamed-5.jpg'
+import photo6 from '../../lewisphotos/unnamed-6.jpg'
+import photo7 from '../../lewisphotos/unnamed-7.jpg'
+import photo8 from '../../lewisphotos/unnamed-8.jpg'
+import photo9 from '../../lewisphotos/unnamed-9.jpg'
+
+
 
 
 
@@ -12,11 +24,14 @@ const Home = () => {
     <>
       <section>
         
-        <h1 className="lewis">Lewis Cleanup Services</h1>
+      
+      
+        <h1 className="lewis animate__animated animate__fadeIn">Lewis Cleanup Services</h1>
+      
 
         <div className="text-center">
 
-          <img className="cleaner" src={cleaner} alt="cleaner"/>
+          <img className="cleaner animate__animated animate__fadeIn" src={cleaner} alt="cleaner"/>
           {/* <img src={solar} alt="cleaner"/> */}
         </div>
 
@@ -29,16 +44,86 @@ const Home = () => {
           </div>
         </div>
 
-        <a name='gallery'></a>
+        <a name='services'></a>
         <div>
-          <Jumbotron className="jumbotron">
-            <h1>hello</h1>
+          <Jumbotron className="jumbotron text-center">
+            <h1>Services:</h1>
+            <Row>
+              <Col md='4'>
+                <h1>Trash Removal</h1>
+              </Col>
+              <Col md='4'>
+                <h1>Solar Panel Cleaning</h1>
+              </Col>
+              <Col md='4'>
+                <h1>Pressure Washing</h1>
+              </Col>
+            </Row>
 
           </Jumbotron>
         </div>
 
 
-      
+        <a name='gallery'></a>
+        <div>
+          <Jumbotron className="gallery text-center">
+            <h1>Photo Gallery:</h1>
+            <Row>
+              <Col md='4'>
+                <img className="photo" src={photo1} alt="photo"/>
+              </Col>
+              <Col md='4'>
+                <img className="photo" src={photo2} alt="photo" />
+              </Col>
+              <Col md='4'>
+                <img className="photo" src={photo4} alt="photo" />
+              </Col>
+            </Row>
+            <Row>
+              <Col md='4'>
+                <img className="photo" src={photo3} alt="photo" />
+              </Col>
+              <Col md='4'>
+                <img className="photo" src={photo5} alt="photo" />
+              </Col>
+              <Col md='4'>
+                <img className="photo" src={photo6} alt="photo"/>
+              </Col>
+            </Row>
+            <Row>
+              <Col md='4'>
+                <img className="photo" src={photo7} alt="photo"/>
+              </Col>
+              <Col md='4'>
+                <img className="photo" src={photo8} alt="photo"/>
+              </Col>
+              <Col md='4'>
+                <img className="photo" src={photo9} alt="photo"/>
+              </Col>
+            </Row>
+
+          </Jumbotron>
+        </div>
+
+      <a name='contact'></a>
+      <div className="text-center">
+        <p className="contact">
+          Have a question about our services? To get a free quote, or if you have questions or special requests, just drop us a line. We look forward to serving you!
+        </p>
+        <p>
+          Email:
+        </p>
+        <p>
+            <a href="mailto:LewisCleanupServices@gmail.com"><p>LewisCleanupServices@gmail.com</p></a>
+        </p>
+        <p>
+          Phone:
+        </p>
+        <p>
+         (951) 385-9635
+        </p>
+          
+      </div>
 
       </section>
     </>
