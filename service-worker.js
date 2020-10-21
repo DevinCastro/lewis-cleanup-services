3c4eb0e5d91ea2f98e0ad5639905bd881a34ae59
Devin Castro <dcrocker21@gmail.com>
2020-10-20 22:13:53 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/lewis-cleanup-services/precache-manifest.8c2b85344ba9640e4a8a91c675703d2b.js"
+  "/lewis-cleanup-services/precache-manifest.3ed3e32f03daba45aff5f5051d400e32.js"
 );
 
 self.addEventListener('message', (event) => {
