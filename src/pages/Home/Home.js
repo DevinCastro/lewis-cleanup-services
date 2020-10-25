bfb0fddeb1ea82d03c34c876df7da500992187d5
DevinCastro <dcrocker21@gmail.com>
2020-10-24 20:34:01 -0700

    deploy

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -46,7 +46,6 @@ const Home = () => {
 
         {/* <h1 className="lewis animate__animated animate__fadeIn">Lewis Clean-up Services</h1> */}
 
-        <h1>Spencer is</h1>
 
         <div className="chris text-center">
 
